b8b8934a6f2b0c2d561d355094ac64f8b2fe3497
pallavisethi <pallavisethi.cse@gmail.com>
2015-04-14 02:56:20 +0530

    comit in between

--- tejas_installation_kit/Tejas-Simulator/Tejas/src/simulator/pipeline/multi_issue_inorder/FetchUnitIn_MII.java
+++ tejas_installation_kit/Tejas-Simulator/Tejas/src/simulator/pipeline/multi_issue_inorder/FetchUnitIn_MII.java
@@ -19,238 +19,202 @@ import generic.PortType;
 import generic.RequestType;
 import generic.SimulationElement;
 
-public class FetchUnitIn_MII extends SimulationElement
-{
+public class FetchUnitIn_MII extends SimulationElement {
 	Core core;
 	MultiIssueInorderExecutionEngine containingExecutionEngine;
-	
+
 	public GenericCircularQueue<Instruction> inputToPipeline;
 	StageLatch_MII ifId_latch;
-	
+
 	Instruction fetchBuffer[];
 	public int fetchBufferCapacity;
-	private int fetchFillCount;	//Number of instructions in the fetch buffer
-	private int fetchBufferIndex;	//Index to first instruction to be popped out of fetch buffer
-	private boolean fetchBufferStatus[];  // To check whether request to ICache is complete or not
-	
-	private boolean sleep;		//The boolean to stall the pipeline when a sync request is received
+	private int fetchFillCount; // Number of instructions in the fetch buffer
+	private int fetchBufferIndex; // Index to first instruction to be popped out of fetch buffer
+	private boolean fetchBufferStatus[]; // To check whether request to ICache is complete or not
+
+	private boolean sleep; // The boolean to stall the pipeline when a sync request is received
 	int syncCount;
 	long numRequestsSent;
 	int numRequestsAcknowledged;
-	
-	long instCtr; //for debug
 
-	
-	public FetchUnitIn_MII(Core core, EventQueue eventQueue, MultiIssueInorderExecutionEngine execEngine)
-	{
+	long instCtr; // for debug
+
+	public FetchUnitIn_MII(Core core, EventQueue eventQueue, MultiIssueInorderExecutionEngine execEngine) {
 		super(PortType.Unlimited, -1, -1, -1, -1);
-		
+
 		this.core = core;
 		this.containingExecutionEngine = execEngine;
-		
+
 		this.ifId_latch = execEngine.getIfIdLatch();
-		
-		this.fetchBufferCapacity = (int)(core.getIssueWidth()
-				* (SystemConfig.core[core.getCore_number()].getICacheLatency()));
+
+		this.fetchBufferCapacity = (int) (core.getIssueWidth() * (SystemConfig.core[core.getCore_number()].getICacheLatency()));
 		this.fetchBuffer = new Instruction[this.fetchBufferCapacity];
-		this.fetchFillCount=0;
-		this.fetchBufferIndex=0;
+		this.fetchFillCount = 0;
+		this.fetchBufferIndex = 0;
 		this.fetchBufferStatus = new boolean[this.fetchBufferCapacity];
-		for(int i=0;i<this.fetchBufferCapacity;i++)
-		{
-			this.fetchBufferStatus[i]=false;
+		for (int i = 0; i < this.fetchBufferCapacity; i++) {
+			this.fetchBufferStatus[i] = false;
 		}
-		
-		this.sleep=false;
-		this.syncCount=0;
-		this.numRequestsSent=0;
-		this.numRequestsAcknowledged=0;
+
+		this.sleep = false;
+		this.syncCount = 0;
+		this.numRequestsSent = 0;
+		this.numRequestsAcknowledged = 0;
 		instCtr = 0;
 	}
-	
-	public void fillFetchBuffer(MultiIssueInorderPipeline inorderPipeline)
-	{
-		if(inputToPipeline.isEmpty())
+
+	public void fillFetchBuffer(MultiIssueInorderPipeline inorderPipeline) {
+		if (inputToPipeline.isEmpty())
 			return;
-		
-		Instruction newInstruction=null;
-		for(int i=(this.fetchBufferIndex+this.fetchFillCount)%this.fetchBufferCapacity;this.fetchFillCount<this.fetchBufferCapacity
-				;i = (i+1)%this.fetchBufferCapacity){
-			
-			if(containingExecutionEngine.multiIssueInorderCoreMemorySystem.getiCache().isBusy()){
+
+		Instruction newInstruction = null;
+		for (int i = (this.fetchBufferIndex + this.fetchFillCount) % this.fetchBufferCapacity; this.fetchFillCount < this.fetchBufferCapacity; i = (i + 1)
+				% this.fetchBufferCapacity) {
+
+			if (containingExecutionEngine.multiIssueInorderCoreMemorySystem.getiCache().isBusy()) {
 				break;
 			}
-			
+
 			newInstruction = inputToPipeline.pollFirst();
-			
-			if(newInstruction == null)
+
+			if (newInstruction == null)
 				return;
 			numRequestsSent++;
-			if(newInstruction.getOperationType() == OperationType.inValid) {
+			if (newInstruction.getOperationType() == OperationType.inValid) {
 				this.fetchBuffer[i] = newInstruction;
-				this.fetchBufferStatus[i]=true;
+				this.fetchBufferStatus[i] = true;
 				this.fetchFillCount++;
 			}
-			
-			else
-			{
-				this.fetchBuffer[i]= newInstruction;
+
+			else {
+				this.fetchBuffer[i] = newInstruction;
 				this.fetchFillCount++;
-				if(newInstruction.getOperationType() != OperationType.sync)
-				{
+				if (newInstruction.getOperationType() != OperationType.sync) {
 					newInstruction.setSerialNo(instCtr++);
 				}
 
-				// The first micro-operation of an instruction has a valid CISC IP. All the subsequent 
+				// The first micro-operation of an instruction has a valid CISC IP. All the subsequent
 				// micro-ops will have IP = -1(meaning invalid). We must not forward this requests to iCache.
-				if(SimulationConfig.detachMemSysInsn || newInstruction.getCISCProgramCounter()==-1)
-				{
-					this.fetchBufferStatus[i]=true;
-				}
-				else
-				{
-					this.fetchBufferStatus[i]=false;
-					containingExecutionEngine.multiIssueInorderCoreMemorySystem.issueRequestToInstrCache(
-							newInstruction.getCISCProgramCounter());
+				if (SimulationConfig.detachMemSysInsn || newInstruction.getCISCProgramCounter() == -1) {
+					this.fetchBufferStatus[i] = true;
+				} else {
+					this.fetchBufferStatus[i] = false;
+					containingExecutionEngine.multiIssueInorderCoreMemorySystem.issueRequestToInstrCache(newInstruction.getCISCProgramCounter());
 				}
 			}
 		}
 	}
-	
-	public void performFetch(MultiIssueInorderPipeline inorderPipeline)
-	{
-		if(containingExecutionEngine.getMispredStall() > 0)
-		{
+
+	public void performFetch(MultiIssueInorderPipeline inorderPipeline) {
+		if (containingExecutionEngine.getMispredStall() > 0) {
 			containingExecutionEngine.decrementMispredStall(1);
 			return;
 		}
-		
+
 		Instruction ins;
-			
-		if(!this.fetchBufferStatus[this.fetchBufferIndex])
-			containingExecutionEngine.incrementInstructionMemStall(1); 
 
-		//move to the IF-ID latch those instructions that have completed
-		//fetch from the i-cache
-		while(!this.sleep && this.fetchFillCount > 0
-				&& this.fetchBufferStatus[this.fetchBufferIndex]
-				&& this.ifId_latch.isFull() == false)
-	        	{
-					ins = this.fetchBuffer[this.fetchBufferIndex];
+		if (!this.fetchBufferStatus[this.fetchBufferIndex])
+			containingExecutionEngine.incrementInstructionMemStall(1);
 
-					if(ins.getOperationType()==OperationType.sync)
-					{
-						this.fetchFillCount--;
-						this.fetchBufferIndex = (this.fetchBufferIndex+1)%this.fetchBufferCapacity;
-						long barrierAddress = ins.getCISCProgramCounter();
-						Barrier bar = BarrierTable.barrierList.get(barrierAddress);
-						bar.incrementThreads();
-						if(this.core.TreeBarrier == true){
-							setSleep(true);
-							int coreId = this.core.getCore_number();
-							ArchitecturalComponent.coreBroadcastBus.getPort().put(new AddressCarryingEvent(
-									0,this.core.eventQueue,
-									 this.core.barrier_latency,
-									 ArchitecturalComponent.coreBroadcastBus, 
-									 ArchitecturalComponent.coreBroadcastBus, 
-									 RequestType.TREE_BARRIER, 
-									 barrierAddress,
-									 coreId));
+		// move to the IF-ID latch those instructions that have completed
+		// fetch from the i-cache
+		while (!this.sleep && this.fetchFillCount > 0 && this.fetchBufferStatus[this.fetchBufferIndex] && this.ifId_latch.isFull() == false) {
+			ins = this.fetchBuffer[this.fetchBufferIndex];
+
+			if (ins.getOperationType() == OperationType.sync) {
+				this.fetchFillCount--;
+				this.fetchBufferIndex = (this.fetchBufferIndex + 1) % this.fetchBufferCapacity;
+				long barrierAddress = ins.getCISCProgramCounter();
+				Barrier bar = BarrierTable.barrierList.get(barrierAddress);
+				bar.incrementThreads();
+				if (this.core.TreeBarrier == true) {
+					setSleep(true);
+					int coreId = this.core.getCore_number();
+					ArchitecturalComponent.coreBroadcastBus.getPort().put(
+							new AddressCarryingEvent(0, this.core.eventQueue, this.core.barrier_latency, ArchitecturalComponent.coreBroadcastBus,
+									ArchitecturalComponent.coreBroadcastBus, RequestType.TREE_BARRIER, barrierAddress, coreId));
+				} else {
+					if (bar.timeToCross()) {
+						sleepThePipeline();
+						int bar_lat;
+
+						if (this.core.barrierUnit == 0) {
+							if (GlobalClock.getCurrentTime() < bar.time + 35) {
+								bar_lat = (int) (this.core.barrier_latency + GlobalClock.getCurrentTime() - bar.time);
+							} else
+								bar_lat = this.core.barrier_latency;
+						} else {
+							if (GlobalClock.getCurrentTime() < bar.time + 4) {
+								bar_lat = (int) (this.core.barrier_latency + GlobalClock.getCurrentTime() - bar.time);
+							} else
+								bar_lat = this.core.barrier_latency;
 						}
-						else{
-							if(bar.timeToCross())
-							{
-								sleepThePipeline();
-								int bar_lat;
-								
-								if(this.core.barrierUnit == 0){
-									if(GlobalClock.getCurrentTime() < bar.time + 35)
-									{
-										bar_lat = (int)(this.core.barrier_latency + GlobalClock.getCurrentTime() - bar.time);
-									}
-									else
-										bar_lat = this.core.barrier_latency;
-								}
-								else{
-									if(GlobalClock.getCurrentTime() < bar.time + 4)
-									{
-										bar_lat = (int)(this.core.barrier_latency + GlobalClock.getCurrentTime() - bar.time);
-									}
-									else
-										bar_lat = this.core.barrier_latency;
-								}
-								for(int i=0; i<bar.getNumThreads(); i++ ){
-									ArchitecturalComponent.coreBroadcastBus.addToResumeCore(bar.getBlockedThreads().elementAt(i));
-								}
-								ArchitecturalComponent.coreBroadcastBus.getPort().put(new AddressCarryingEvent(
-										 this.core.eventQueue,
-										 bar_lat,
-										 ArchitecturalComponent.coreBroadcastBus, 
-										 ArchitecturalComponent.coreBroadcastBus, 
-										 RequestType.PIPELINE_RESUME, 
-										 0));
-	
-							}
-							else
-							{
-								sleepThePipeline();
-								return;
-							}
+						for (int i = 0; i < bar.getNumThreads(); i++) {
+							ArchitecturalComponent.coreBroadcastBus.addToResumeCore(bar.getBlockedThreads().elementAt(i));
 						}
+						ArchitecturalComponent.coreBroadcastBus.getPort().put(
+								new AddressCarryingEvent(this.core.eventQueue, bar_lat, ArchitecturalComponent.coreBroadcastBus, ArchitecturalComponent.coreBroadcastBus,
+										RequestType.PIPELINE_RESUME, 0));
+
+					} else {
+						sleepThePipeline();
+						return;
 					}
-					else{
-						this.ifId_latch.add(ins, GlobalClock.getCurrentTime() + 1);
-						this.fetchFillCount--;
-						this.fetchBuffer[this.fetchBufferIndex] = null;
-						this.fetchBufferStatus[this.fetchBufferIndex] = false;
-						this.fetchBufferIndex = (this.fetchBufferIndex+1)%this.fetchBufferCapacity;
-						
-						if(SimulationConfig.debugMode)
-						{
-							System.out.println("fetched : " + GlobalClock.getCurrentTime()/core.getStepSize() + "\n"  + ins + "\n");
-						}
-					}
+				}
+			} else {
+				this.ifId_latch.add(ins, GlobalClock.getCurrentTime() + 1);
+				this.fetchFillCount--;
+				this.fetchBuffer[this.fetchBufferIndex] = null;
+				this.fetchBufferStatus[this.fetchBufferIndex] = false;
+				this.fetchBufferIndex = (this.fetchBufferIndex + 1) % this.fetchBufferCapacity;
+
+				if (SimulationConfig.debugMode) {
+					System.out.println("fetched : " + GlobalClock.getCurrentTime() / core.getStepSize() + "\n" + ins + "\n");
+				}
 			}
-		
-		//remove micro-ops from inputToPipeline and issue request to the i-cache
+		}
+
+		// remove micro-ops from inputToPipeline and issue request to the i-cache
 		fillFetchBuffer(inorderPipeline);
 	}
-	
-	public void setSleep(boolean _sleep){
-		this.sleep=_sleep;
+
+	public void setSleep(boolean _sleep) {
+		this.sleep = _sleep;
 	}
-	public boolean getSleep(){
+
+	public boolean getSleep() {
 		return this.sleep;
 	}
-	public void sleepThePipeline(){
-		System.out.println("sleeping pipeline" + this.core.getCore_number()+ "...!!");
+
+	public void sleepThePipeline() {
+		System.out.println("sleeping pipeline" + this.core.getCore_number() + "...!!");
 		this.syncCount--;
-		this.sleep=true;
-	}	
-	public GenericCircularQueue<Instruction> getInputToPipeline(){
+		this.sleep = true;
+	}
+
+	public GenericCircularQueue<Instruction> getInputToPipeline() {
 		return this.inputToPipeline;
 	}
-	public void setInputToPipeline(GenericCircularQueue<Instruction> inpList){
+
+	public void setInputToPipeline(GenericCircularQueue<Instruction> inpList) {
 		this.inputToPipeline = inpList;
 	}
-	public void resumePipeline(){
-		System.out.println("Resuming the pipeline "+this.core.getCore_number() + "...!!");
+
+	public void resumePipeline() {
+		System.out.println("Resuming the pipeline " + this.core.getCore_number() + "...!!");
 		this.syncCount++;
-		this.sleep=false;
+		this.sleep = false;
 	}
 
 	@Override
 	public void handleEvent(EventQueue eventQ, Event event) {
-				
+
 	}
-	
-	public void processCompletionOfMemRequest(long requestedAddress)
-	{
-		for(int i=0;i<this.fetchBufferCapacity;i++){
-			if(this.fetchBuffer[i] != null && 
-					this.fetchBuffer[i].getCISCProgramCounter() == requestedAddress && 
-					this.fetchBufferStatus[i]==false){
-				this.fetchBufferStatus[i]=true;
+
+	public void processCompletionOfMemRequest(long requestedAddress) {
+		for (int i = 0; i < this.fetchBufferCapacity; i++) {
+			if (this.fetchBuffer[i] != null && this.fetchBuffer[i].getCISCProgramCounter() == requestedAddress && this.fetchBufferStatus[i] == false) {
+				this.fetchBufferStatus[i] = true;
 			}
 		}
 	}
--- tejas_installation_kit/Tejas-Simulator/Tejas/src/simulator/pipeline/multi_issue_inorder/Toma_Issue.java
+++ tejas_installation_kit/Tejas-Simulator/Tejas/src/simulator/pipeline/multi_issue_inorder/Toma_Issue.java
@@ -3,7 +3,11 @@
  */
 package pipeline.multi_issue_inorder;
 
+import pipeline.FunctionalUnitType;
+import pipeline.OpTypeToFUTypeMapping;
 import generic.Core;
+import generic.GenericCircularQueue;
+import generic.Instruction;
 
 /**
  * @author dell
@@ -11,14 +15,40 @@ import generic.Core;
  */
 public class Toma_Issue {
 	// TODO:--- check whether to extend simulation element
+	// TODO: --- check do we need latch
 
 	Core core;
 
+	public GenericCircularQueue<Instruction> inputToPipeline;
+
+	// TODO: check this is a single queue rather a array as in OOO
+
 	public Toma_Issue(Core core) {
+		// TODO: check do we need "super(PortType.Unlimited, -1, -1, -1, -1);"... i think hona chahiye
 		this.core = core;
 	}
 
 	public void performIssue() {
-		// TODO: get instruction from instruction Queue
+		// TODO: check do we need icacheBuffer
+		// TODO: logic in inOrder & OOO -- sir se upar ja ra hai... check later
+
+		if (inputToPipeline.isEmpty())
+			return;
+
+		Instruction ins = inputToPipeline.pollFirst();
+
+		if (ins == null)
+			return;
+
+		// TODO: shall be some logic if instr is invalid
+
+		long fURequest = 0;
+		if (OpTypeToFUTypeMapping.getFUType(ins.getOperationType()) != FunctionalUnitType.inValid) {
+			fURequest = containingExecutionEngine.getExecutionCore().requestFU(OpTypeToFUTypeMapping.getFUType(ins.getOperationType()));
+
+			if (fURequest > 0) {
+				break;
+			}
+		}
 	}
 }
